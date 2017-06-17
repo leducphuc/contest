737240f96c4b0b0c68bd30d247ccc5da081c368a
ducphucle <leducphuc1994@gmail.com>
2017-06-18 06:59:13 +0700

    2

--- contet.java
+++ contet.java
@@ -33,3 +33,59 @@ for(int i = 0; i< array.length; i++){
                }
             }    
         }
+
+public class MainClass {
+   public static long fibonacci(long number) {
+      if ((number == 0) || (number == 1))
+         return number;
+      else
+         return fibonacci(number - 1) + fibonacci(number - 2);
+   }
+   public static void main(String[] args) {
+      for (int counter = 0; counter <= 10; counter++){
+         System.out.printf("Fibonacci of %d is: %d\n",
+         counter, fibonacci(counter));
+      }
+   }
+}
+
+
+package chuyende1;
+
+import java.util.Scanner;
+
+public class A {
+
+	static int T,N;
+	static int[] A;
+	
+	static boolean doixung(){
+		int i=0,j=N-1;
+		while(i<j){
+			if(A[i]!=A[j]) return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		T=sc.nextInt();
+		for(int test=1;test<=T;test++){
+			N=sc.nextInt();
+			A=new int[N];
+			for(int i=0;i<N;i++){
+				A[i]=sc.nextInt();
+			}
+			if(doixung()){
+				System.out.println("YES");
+			}
+			else{
+				System.out.println("NO");
+			}
+		}
+	}
+}
+
+
